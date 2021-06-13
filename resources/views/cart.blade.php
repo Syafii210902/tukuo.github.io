d6653dc1e47f7b1ef5986a2b79d82751fe904d72
qsaifudin <duafinis@gmail.com>
2021-06-13 21:54:52 +0700

    aa

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,236 +1,119 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app-user')
+@section('content')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>IndoMarket - Free E-Commerce Website Template built with Boostrap 4 and Argon Design System</title>
-
-    <!-- Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
-        rel="stylesheet">
-
-    <!-- Icons -->
-    <link href="./assets/css/nucleo-icons.css" rel="stylesheet">
-    <link href="./assets/css/font-awesome.css" rel="stylesheet">
-
-    <!-- Jquery UI -->
-    <link type="text/css" href="./assets/css/jquery-ui.css" rel="stylesheet">
-
-    <!-- Argon CSS -->
-    <link type="text/css" href="./assets/css/argon-design-system.min.css" rel="stylesheet">
-
-    <!-- Main CSS-->
-    <link type="text/css" href="./assets/css/style.css" rel="stylesheet">
-
-    <!-- Optional Plugins-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-</head>
-
-<body>
-    <header class="header clearfix">
-        <div class="top-bar d-none d-sm-block">
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 text-left">
-                        <ul class="top-links contact-info">
-                            <li><i class="fa fa-envelope-o"></i> <a href="#">contact@example.com</a></li>
-                            <li><i class="fa fa-whatsapp"></i> +1 5589 55488 55</li>
-                        </ul>
-                    </div>
-                    <div class="col-6 text-right">
-                        <ul class="top-links account-links">
-                            <li><i class="fa fa-user-circle-o"></i> <a href="#">My Account</a></li>
-                            <li><i class="fa fa-power-off"></i> <a href="#">Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="header-main border-top">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-3 col-12 col-sm-6">
-                        <a class="navbar-brand mr-lg-5" href="./">
-                            <i class="fa fa-shopping-bag fa-3x"></i> <span class="logo">TukuO</span>
-                        </a>
-                    </div>
-                    <div class="col-lg-7 col-12 col-sm-6">
-                        <form action="#" class="search">
-                            <div class="input-group w-100">
-                                <input type="text" class="form-control" placeholder="Search">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" type="submit">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-lg-2 col-12 col-sm-6">
-                        <div class="right-icons pull-right d-none d-lg-block">
-                            <div class="single-icon wishlist">
-                                <a href="#"><i class="fa fa-heart-o fa-2x"></i></a>
-                                <span class="badge badge-default">5</span>
-                            </div>
-                            <div class="single-icon shopping-cart">
-                                <a href="cart"><i class="fa fa-shopping-cart fa-2x"></i></a>
-                                <span class="badge badge-default">4</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <nav class="navbar navbar-main navbar-expand-lg navbar-light border-top border-bottom">
-            <div class="container">
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav"
-                    aria-controls="main_nav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="main_nav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="./">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#"
-                                aria-expanded="true">Pages</a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="products">Products</a>
-                                <a class="dropdown-item" href="product-detail">Product Detail</a>
-                                <a class="dropdown-item" href="cart">Cart</a>
-                                <a class="dropdown-item" href="checkout">Checkout</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <!-- collapse .// -->
-            </div>
-            <!-- container .// -->
-        </nav>
-    </header>
-    <!------------------------------------------
+<!------------------------------------------
     Page Header
     ------------------------------------------->
-    <section class="breadcrumb-section pb-3 pt-3">
-        <div class="container">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="./">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Cart</li>
-            </ol>
-        </div>
-    </section>
-    <section class="shoping-cart spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="shoping__cart__table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th class="shoping__product">Products</th>
-                                    <th>Price</th>
-                                    <th>Quantity</th>
-                                    <th>Total</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="shoping__cart__item">
-                                        <img src="assets/img/products/p5.png" alt="">
-                                        <h5>Vegetable’s Package</h5>
-                                    </td>
-                                    <td class="shoping__cart__price">
-                                        $55.00
-                                    </td>
-                                    <td class="shoping__cart__quantity">
-                                        <div class="quantity">
-                                            <div class="pro-qty"><span class="dec qtybtn">-</span>
-                                                <input type="text" value="1">
-                                                <span class="inc qtybtn">+</span></div>
-                                        </div>
-                                    </td>
-                                    <td class="shoping__cart__total">
-                                        $110.00
-                                    </td>
-                                    <td class="shoping__cart__item__close">
-                                        <span class="icon_close"></span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="shoping__cart__item">
-                                        <img src="assets/img/products/p5.png" alt="">
-                                        <h5>Fresh Garden Vegetable</h5>
-                                    </td>
-                                    <td class="shoping__cart__price">
-                                        $39.00
-                                    </td>
-                                    <td class="shoping__cart__quantity">
-                                        <div class="quantity">
-                                            <div class="pro-qty"><span class="dec qtybtn">-</span>
-                                                <input type="text" value="1">
-                                                <span class="inc qtybtn">+</span></div>
-                                        </div>
-                                    </td>
-                                    <td class="shoping__cart__total">
-                                        $39.99
-                                    </td>
-                                    <td class="shoping__cart__item__close">
-                                        <span class="icon_close"></span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="shoping__cart__item">
-                                        <img src="assets/img/products/p5.png" alt="">
-                                        <h5>Organic Bananas</h5>
-                                    </td>
-                                    <td class="shoping__cart__price">
-                                        $69.00
-                                    </td>
-                                    <td class="shoping__cart__quantity">
-                                        <div class="quantity">
-                                            <div class="pro-qty"><span class="dec qtybtn">-</span>
-                                                <input type="text" value="1">
-                                                <span class="inc qtybtn">+</span></div>
-                                        </div>
-                                    </td>
-                                    <td class="shoping__cart__total">
-                                        $69.99
-                                    </td>
-                                    <td class="shoping__cart__item__close">
-                                        <span class="icon_close"></span>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                </div>
-                <div class="col-lg-6">
-                    <div class="shoping__checkout">
-                        <h5>Cart Total</h5>
-                        <ul>
-                            <li>Subtotal <span>$454.98</span></li>
-                            <li>Total <span>$454.98</span></li>
-                        </ul>
-                        <a href="#" class="primary-btn">PROCEED TO CHECKOUT</a>
-                    </div>
+<section class="breadcrumb-section pb-3 pt-3">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="./">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Cart</li>
+        </ol>
+    </div>
+</section>
+<section class="shoping-cart spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="shoping__cart__table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th class="shoping__product">Products</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="shoping__cart__item">
+                                    <img src="assets/img/products/p5.png" alt="">
+                                    <h5>Vegetable’s Package</h5>
+                                </td>
+                                <td class="shoping__cart__price">
+                                    $55.00
+                                </td>
+                                <td class="shoping__cart__quantity">
+                                    <div class="quantity">
+                                        <div class="pro-qty"><span class="dec qtybtn">-</span>
+                                            <input type="text" value="1">
+                                            <span class="inc qtybtn">+</span></div>
+                                    </div>
+                                </td>
+                                <td class="shoping__cart__total">
+                                    $110.00
+                                </td>
+                                <td class="shoping__cart__item__close">
+                                    <span class="icon_close"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="shoping__cart__item">
+                                    <img src="assets/img/products/p5.png" alt="">
+                                    <h5>Fresh Garden Vegetable</h5>
+                                </td>
+                                <td class="shoping__cart__price">
+                                    $39.00
+                                </td>
+                                <td class="shoping__cart__quantity">
+                                    <div class="quantity">
+                                        <div class="pro-qty"><span class="dec qtybtn">-</span>
+                                            <input type="text" value="1">
+                                            <span class="inc qtybtn">+</span></div>
+                                    </div>
+                                </td>
+                                <td class="shoping__cart__total">
+                                    $39.99
+                                </td>
+                                <td class="shoping__cart__item__close">
+                                    <span class="icon_close"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="shoping__cart__item">
+                                    <img src="assets/img/products/p5.png" alt="">
+                                    <h5>Organic Bananas</h5>
+                                </td>
+                                <td class="shoping__cart__price">
+                                    $69.00
+                                </td>
+                                <td class="shoping__cart__quantity">
+                                    <div class="quantity">
+                                        <div class="pro-qty"><span class="dec qtybtn">-</span>
+                                            <input type="text" value="1">
+                                            <span class="inc qtybtn">+</span></div>
+                                    </div>
+                                </td>
+                                <td class="shoping__cart__total">
+                                    $69.99
+                                </td>
+                                <td class="shoping__cart__item__close">
+                                    <span class="icon_close"></span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-    </section>
-</body>
-
-</html>
+        <div class="row">
+            <div class="col-lg-6">
+            </div>
+            <div class="col-lg-6">
+                <div class="shoping__checkout">
+                    <h5>Cart Total</h5>
+                    <ul>
+                        <li>Subtotal <span>$454.98</span></li>
+                        <li>Total <span>$454.98</span></li>
+                    </ul>
+                    <a href="#" class="primary-btn">PROCEED TO CHECKOUT</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
